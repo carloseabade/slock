243a70d8147423e47d2fb68069faf0f6a80cf477
Carlos Abade <carloseabade@gmail.com>
2023-05-01 17:46:23 -0300

    feat: add colormessage

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 simple screen locker utility for X. It's one of suckless utilities, more info [here](https://tools.suckless.org/slock/).
 
+## Patches
+
+As this is my own build, I've added some patches to it.
+
+- [colormessage](https://tools.suckless.org/slock/patches/colormessage/)
+
 ## Requirements
 
 In order to build slock you need the Xlib header files.
--- config.def.h
+++ config.def.h
@@ -4,9 +4,18 @@ static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "#282828",     /* after initialization */
-	[INPUT] =  "#7c6f64",   /* during input */
+	[INPUT] =  "#282828",   /* during input */
 	[FAILED] = "#fe8019",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* default message */
+static const char * message = "Welcome back, Carlos. Type your password";
+
+/* text color */
+static const char * text_color = "#fe8019";
+
+/* text size (must be a valid size) */
+static const char * font_name = "6x13";
