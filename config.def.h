5f2997b7200f50d164ff998d04c3200ac5f37b12
Carlos Abade <carloseabade@gmail.com>
2023-07-06 19:07:15 -0300

    feat: arch red color

--- config.def.h
+++ config.def.h
@@ -3,19 +3,19 @@ static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "#282828",     /* after initialization */
-	[INPUT] =  "#282828",   /* during input */
-	[FAILED] = "#fe8019",   /* wrong password */
+	[INIT] =   "#212320",     /* after initialization */
+	[INPUT] =  "#212320",   /* during input */
+	[FAILED] = "#a21212",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
 
 /* default message */
-static const char * message = "Welcome back, Carlos. Type your password";
+static const char * message = "Locked";
 
 /* text color */
-static const char * text_color = "#fe8019";
+static const char * text_color = "#dddddd";
 
 /* text size (must be a valid size) */
 static const char * font_name = "6x13";
